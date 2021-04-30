fb298a151e84ed2a1fdea5c7c3fd7e4b57c63cbc
sheik syed abthaheer <sheiksyedm@syncfusion.com>
2021-04-30 13:43:09 +0530

    Choropleth Maps sample added.

---
+++ Choropleth_Map/Choropleth_Map/Choropleth_Map/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Choropleth_Map
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageryLayer_RequestTileUri(object sender, Syncfusion.SfMaps.XForms.TileUriArgs e)
+        {
+            var link = "http://mt1.google.com/vt/lyrs=y&x=" + e.X.ToString() + "&y=" + e.Y.ToString() + "&z=" + e.ZoomLevel.ToString();
+            e.Uri = link;
+        }
+    }
+}
